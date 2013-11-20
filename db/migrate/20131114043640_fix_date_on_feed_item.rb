ef5e00106444eeff47f348b7e25fbaff04fa52e1
Jason Habing <jasonhabing@gmail.com>
2013-11-19 19:33:09 -0800

    fixed end error

--- db/migrate/20131114043640_fix_date_on_feed_item.rb
+++ db/migrate/20131114043640_fix_date_on_feed_item.rb
@@ -2,7 +2,6 @@ class FixDateOnFeedItem < ActiveRecord::Migration
   def up
     remove_column :feed_items, :published
     add_column :feed_items, :published, :datetime
-    end
   end
 
   def down
