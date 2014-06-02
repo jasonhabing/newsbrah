a5603ae86028670cfd0fc35afcfad9307d9d123a
Jason Habing <jasonhabing@gmail.com>
2014-06-01 17:34:10 -0700

    database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ NewsApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  ENV["HEROKU_POSTGRESQL_ORANGE_URL"] = "postgres://ahkdygrpsfeuov:8UggwR-K064AQ2I_2KLuWOYqUa@ec2-23-23-80-55.compute-1.amazonaws.com:5432/daclaaekqlvulo"
+
+
 end
